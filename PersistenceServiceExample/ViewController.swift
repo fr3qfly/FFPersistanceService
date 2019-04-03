0e0067d335c782f349d134eae75a0fe2d67d2d56
BalazsSzamody <bszamody@gmail.com>
2019-04-03 17:52:57 +1100

    Initial Commit

---
+++ PersistenceServiceExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersistenceServiceExample
+//
+//  Created by Balazs Szamody on 3/4/19.
+//  Copyright © 2019 Fr3qFly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
